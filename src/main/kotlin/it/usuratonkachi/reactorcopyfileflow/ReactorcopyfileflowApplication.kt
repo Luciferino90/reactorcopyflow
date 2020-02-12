a705fedb7ef1511fb9c49e126ab68ee897b74532
Luciferino <luca.fanciullini@gmail.com>
2020-02-12 20:55:12 +0100

    first commit

---
+++ src/main/kotlin/it/usuratonkachi/reactorcopyfileflow/ReactorcopyfileflowApplication.kt
@@ -0,0 +1,11 @@
+package it.usuratonkachi.reactorcopyfileflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactorcopyfileflowApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactorcopyfileflowApplication>(*args)
+}
---
+++ src/test/kotlin/it/usuratonkachi/reactorcopyfileflow/ReactorcopyfileflowApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.usuratonkachi.reactorcopyfileflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactorcopyfileflowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
